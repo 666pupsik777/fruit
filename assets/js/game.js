ffb4035812bdd4899052f58ac365199b621c1f94
666pupsik777 <pospelova.02.10.06@mail.ru>
2025-10-06 11:48:33 +0700

    счетчик

--- assets/js/game.js
+++ assets/js/game.js
@@ -30,6 +30,57 @@ class Drawable {
             height: ${this.h}px;
         `;
     }
+
+    isCollision(element) {
+    let a = {
+        x: this.x,
+        y: this.y,
+        x2: this.x + this.w,
+        x1: this.y + this.h
+    };
+
+    let b = {
+        x: element.x,
+        y: element.y,
+        x2: element.x + element.w,
+        x1: element.y + element.h
+    }
+    return a.x1 < b.x2 && b.x1 < a.x2 && a.y1 < b.y2 && b.y1 <a.y2;
+}
+
+    class Fruit extends Drawable {
+        constructor(game) {
+            super(game);
+            this.w = 70;
+            this.h = 70;
+            this.y = 60;
+            this.x = random(0, window.innerWidth - this.w);
+            this.offsets.y = 3;
+            this.createElement()
+        }
+        update() {
+        if(this.isCollision(this.game.player)) console.log('plus point');
+        if(this.y > window.innerHeight) console.log('minus hp');
+    }
+
+}
+class Banana extends Fruit {
+    constructor(game) {
+        super(game);
+    }
+}
+
+class Apple extends Fruit {
+    constructor(game) {
+        super(game);
+        this.offsets.y = 5;
+    }
+}
+class Orange extends Fruit {
+    constructor(game) {
+        super(game);
+        this.offsets.y = 7;
+    }
 }
 
 class Player extends Drawable {
@@ -63,9 +114,6 @@ class Player extends Drawable {
         else  this.offsets.x = 0;
         super.update();
     }
-
-
-
 }
 
 class Game {
@@ -73,6 +121,8 @@ class Game {
         this.name = name;
         this.elements = [];
         this.player = this.generate(Player);
+        this.counterForTimer = 0;
+        this.fruits = [Apple, Banana, Orange]
     }
 
     start () {
@@ -87,10 +137,18 @@ class Game {
 
     loop() {
         requestAnimationFrame(() => {
+            this.counterForTimer++;
+            if( this.counterForTimer % 60 === 0) {
+                this.randomFruitGenerate();
+            }
             this.updateElements();
             this.setParams();
             this.loop();
         });
+    }
+
+    randomFruitGenerate() {
+            this.generate(this.fruits[random(0,2)])
     }
 
     updateElements() {
